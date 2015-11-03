e49ff2bd4537bc753257cc68ddc37c6ea19e2034
Victor W <vwalldev@gmail.com>
2015-11-02 19:35:07 -0800

    Small update to how foreign_key is called out in Comment and Post models.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ActiveRecord::Base
-  belongs_to :user
+  # Need to specify foreign key and class name for belongs_to because the column name is "user_id" instead of the default of "creator"
+  belongs_to :creator, foreign_key: 'user_id', class_name: 'User'
   has_many :comments
 end
