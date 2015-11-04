473436579230222fe16e156cd398d116bb54cf80
Victor W <vwalldev@gmail.com>
2015-11-04 09:27:41 -0800

    Finished M:M between Post and Category

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,6 @@ class Post < ActiveRecord::Base
   # Need to specify foreign key and class name for belongs_to because the column name is "user_id" instead of the default of "creator"
   belongs_to :creator, foreign_key: 'user_id', class_name: 'User'
   has_many :comments
+  has_many :post_categories
+  has_many :categories, through: :post_categories
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151028042710) do
+ActiveRecord::Schema.define(version: 20151104040254) do
 
   create_table "categories", force: true do |t|
     t.string   "name"
@@ -24,6 +24,13 @@ ActiveRecord::Schema.define(version: 20151028042710) do
     t.text     "body"
     t.integer  "user_id"
     t.integer  "post_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "post_categories", force: true do |t|
+    t.integer  "post_id"
+    t.integer  "category_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
