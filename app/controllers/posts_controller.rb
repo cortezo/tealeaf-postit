ab02c7e0aaf175ff44733c49dfd162c76812fd68
Victor W <vwalldev@gmail.com>
2015-12-29 10:59:51 -0800

    PostController update

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,21 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
 
   end
 end
