bb20dedeff94ce439202b45c8e25b5f6d544de64
Victor W <vwalldev@gmail.com>
2015-12-29 14:59:28 -0800

    Finishing out PostsController actions and forms

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,25 +1,50 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update]
+
   def index
     @posts = Post.all
   end
 
-  def show
-    @post = Post.find(params[:id])
-  end
+  def show; end
 
   def new
-
+    @post = Post.new
   end
 
   def create
+    @post = Post.new(post_params)
+    @post.creator = User.first # TEMPORARILY HARD CODED ToDo:  Change once we have authentication
 
+    if @post.save
+      flash[:notice] = "Post successfully created."
+      redirect_to posts_path
+    else
+      render :new
+    end
   end
 
-  def edit
+  def edit; end
+
+  def update
+    if @post.update(post_params)
+      flash[:notice] = "Post successfully updated."
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def set_post
     @post = Post.find(params[:id])
   end
 
-  def update
-
+  def post_params
+    if false  # user.admin?  --- User must be an admin (requires an admin? function that returns t/f)
+         params.require(:post).permit!   # Would permit all attributes to be mass-assigned
+    else
+         params.require(:post).permit(:title, :url, :description)
+    end
   end
 end
